32dcb50b764cf1cd83229bd5aa17b2ae200d562a
Ilijaz Mehmedovic <mehmedovic.ilijaz@gmail.com>
2021-09-08 16:28:52 +0200

    Fully tested typeOf

--- tests/typeof.test.ts
+++ tests/typeof.test.ts
@@ -9,8 +9,42 @@ chai.should();
 @suite
 // @ts-ignore
 class Hello {
-  @test('Type of array should be array')
+  @test('Type of [] should be array')
   typeOfArray() {
     assert.equal(typeOf([]), 'array');
+  }
+
+  @test('Type of "" should be string')
+  typeOfString() {
+    assert.equal(typeOf(''), 'string');
+  }
+
+  @test('Type of undefined should be undefined')
+  typeOfUndefined() {
+    assert.equal(typeOf(undefined), 'undefined');
+  }
+
+  @test('Type of null should be null')
+  typeOfNull() {
+    assert.equal(typeOf(null), 'null');
+  }
+
+  @test('Type of 10 should be number')
+  typeOfNumber() {
+    assert.equal(typeOf(10), 'number');
+  }
+
+  @test('Type of a class should be class')
+  typeOfClass() {
+    class A {}
+    assert.equal(typeOf(A), 'class');
+  }
+
+  @test('Type of a function should be function')
+  typeOfFunction() {
+    assert.equal(
+      typeOf(() => {}),
+      'function'
+    );
   }
 }
